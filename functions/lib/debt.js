c2e961f921e9a9e8b09a319692c39e9cf7c292b9
alva@suten.io <saxis@Saxiss-MacBook-Pro.local>
2019-01-07 22:37:57 -0700

    Removed page design text

---
+++ functions/lib/debt.js
@@ -0,0 +1,38 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const db = admin.firestore();
+exports.debtIncrement = functions.firestore
+    .document('debts/{debtId}')
+    .onCreate((snapshot, context) => __awaiter(this, void 0, void 0, function* () {
+    const data = snapshot.data();
+    const userRef = db.doc(`users/${data.owner}`);
+    const userSnap = yield userRef.get();
+    const userData = userSnap.data();
+    return userRef.update({
+        debtsCount: userData.debtsCount + 1,
+        debtsTotal: userData.debtsTotal + Number(data.total_amount_due)
+    });
+}));
+exports.debtDecrement = functions.firestore
+    .document('debts/{debtId}')
+    .onDelete((snapshot, context) => __awaiter(this, void 0, void 0, function* () {
+    const data = snapshot.data();
+    const userRef = db.doc(`users/${data.owner}`);
+    const userSnap = yield userRef.get();
+    const userData = userSnap.data();
+    return userRef.update({
+        debtsCount: userData.debtsCount - 1,
+        debtsTotal: userData.debtsTotal - Number(data.total_amount_due)
+    });
+}));
+//# sourceMappingURL=debt.js.map
